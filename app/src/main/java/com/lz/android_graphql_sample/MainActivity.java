edaa5e3996f90f4ab5c386338237b613725820b6
lizhen <lizhen@zhumengyuan.com>
2020-11-26 09:52:57 +0800

    baseline

---
+++ app/src/main/java/com/lz/android_graphql_sample/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lz.android_graphql_sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
